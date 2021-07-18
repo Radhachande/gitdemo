83e15974f30af36a44e153883146205a509efa8e
Radhachande <radhavchande@gmail.com>
2021-07-19 01:44:19 +0530

    Initial commit

--- demo.py
+++ demo.py
@@ -1,1 +1,11 @@
 print("hello dev")
+import requests
+
+url = "https://radhachande7-eval-test.apigee.net/v0/hello"
+
+payload={}
+headers = {}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text)
